52d1bd491e5ea90ec7ddd673b8b0715f13dcf34e
Wesley Moore <wesmooredesign@gmail.com>
2020-10-28 16:43:28 -0700

    Initial Commit

---
+++ SoundWizard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoundWizard
+//
+//  Created by Wes Moore on 10/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
